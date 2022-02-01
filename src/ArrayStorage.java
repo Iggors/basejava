797967d46faeb67d05a8c7dbe6c8d5659c63b243
Igor Kraev <ivk.spb@yandex.ru>
2022-02-01 17:33:12 +0300

    Methods: save, get, delete, clear, getAll, size - were modified

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,27 +4,62 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    // nElems - The counter of not null elements in the storage[]
+    int nElems = 0;
+
     void clear() {
+        for (int i = 0; i < nElems; i++) {
+            if (storage[i] != null){
+                storage[i] = null;
+                nElems = 0;
+            }
+        }
     }
 
     void save(Resume r) {
+        storage[nElems] = r;
+        nElems++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < nElems; i++) {
+            if (storage[i].toString() == uuid)
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int i;
+        for (i = 0; i < nElems; i++)
+            if (uuid == storage[i].toString())
+                break;
+        if (i == nElems)
+            System.out.println("The resume with unique identifier " + uuid + "doesn't exist.");
+        else {
+            for (int k = i; k < nElems; k++)
+                storage[k] = storage[k+1];
+            nElems--;
+            System.out.println("The resume with unique identifier " + uuid + " was succesfully deleted.");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        if (nElems > 0){
+            Resume[] tempStorage = new Resume[nElems];
+            System.arraycopy(storage, 0, tempStorage, 0, nElems);
+            return tempStorage;
+        }
+        else return new Resume[0];
     }
 
     int size() {
-        return 0;
+        if (nElems != 0)
+            return nElems;
+        else return 0;
+
     }
 }
