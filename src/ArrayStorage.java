0c6e96635160e1aa51012b9011e1fb2ae41d2e6d
Igor Kraev <ivk.spb@yandex.ru>
2022-02-03 10:50:02 +0300

    Bug fixes after code review

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,54 +1,74 @@
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
+public class ArrayStorage
+{
     Resume[] storage = new Resume[10000];
 
-    // nElems - The counter of not null elements in the storage[]
-    int nElems = 0;
+    // size - The counter of not null elements in the storage[]
+    int size = 0;
 
-    void clear() {
-        for (int i = 0; i < nElems; i++) {
-            if (storage[i] != null){
-                storage[i] = null;
-                nElems = 0;
-            }
+    void clear()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            storage[i] = null;
         }
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume r)
+    {
         int i;
-        for (i = 0; i < nElems; i++)
-            if (storage[i].toString().equals(r.toString())) {
+        for (i = 0; i < size; i++)
+        {
+            if (storage[i].toString().equals(r.toString()))
+            {
                 System.out.println("The resume with unique identifier " + r.uuid + " already exists.");
                 break;
             }
-        if (i == nElems){
-            storage[nElems] = r;
-            nElems++;
-            System.out.println("The resume with unique identifier " + r.uuid + "was succesfully saved.");
+        }
+        if (i == size)
+        {
+            storage[size] = r;
+            size++;
+            System.out.println("The resume with unique identifier " + r.uuid + " was succesfully saved.");
         }
     }
 
-    Resume get(String uuid) {
-        for (int i = 0; i < nElems; i++) {
+    Resume get(String uuid)
+    {
+        for (int i = 0; i < size; i++)
+        {
             if (storage[i].toString() == uuid)
+            {
                 return storage[i];
+            }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    void delete(String uuid)
+    {
         int i;
-        for (i = 0; i < nElems; i++)
+        for (i = 0; i < size; i++)
+        {
             if (uuid == storage[i].toString())
+            {
                 break;
-        if (i == nElems)
-            System.out.println("The resume with unique identifier " + uuid + "doesn't exist.");
-        else {
-            for (int k = i; k < nElems; k++)
-                storage[k] = storage[k+1];
-            nElems--;
+            }
+        }
+        if (i == size)
+        {
+            System.out.println("The resume with unique identifier " + uuid + " doesn't exist.");
+        }
+        else
+        {
+            for (int k = i; k < size - 1; k++)
+            {
+                storage[k] = storage[k + 1];
+            }
+            size--;
             System.out.println("The resume with unique identifier " + uuid + " was succesfully deleted.");
         }
     }
@@ -56,19 +76,19 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        if (nElems > 0){
-            Resume[] tempStorage = new Resume[nElems];
-            System.arraycopy(storage, 0, tempStorage, 0, nElems);
+    Resume[] getAll()
+    {
+        if (size > 0)
+        {
+            Resume[] tempStorage = new Resume[size];
+            System.arraycopy(storage, 0, tempStorage, 0, size);
             return tempStorage;
         }
         else return new Resume[0];
     }
 
-    int size() {
-        if (nElems != 0)
-            return nElems;
-        else return 0;
-
+    int size()
+    {
+        return size;
     }
 }
