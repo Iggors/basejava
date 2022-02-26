0fd8b71be3249c4bdf0883231a0745773fe32758
Igor Kraev <ivk.spb@yandex.ru>
2022-02-26 13:54:47 +0300

    Add static class members

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[3];
+    private static final int STORAGE_LIMIT = 10000;
+
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
 
     // size - The counter of not null elements in the storage[]
     private static int size = 0;
