5a8a74840b51362b8cf39c48cb3e26d48e4cddff
Igor Kraev <ivk.spb@yandex.ru>
2022-03-25 20:44:44 +0300

    Implemented methods in accordance with the template design method

---
+++ src/ru/basejava/storage/ListStorage.java
@@ -0,0 +1,57 @@
+package ru.basejava.storage;
+
+import ru.basejava.model.Resume;
+
+import java.util.ArrayList;
+
+public class ListStorage extends AbstractStorage {
+    ArrayList<Resume> storage = new ArrayList<>();
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    @Override
+    public Resume[] getAll() {
+        return storage.toArray(new Resume[0]);
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < storage.size(); i++) {
+            if (uuid.equals(storage.get(i).getUuid())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    protected Resume getResume(int index) {
+        return storage.get(index);
+    }
+
+    @Override
+    protected void updateResume(Resume r, int index) {
+        storage.set(index, r);
+    }
+
+    @Override
+    protected void saveResume(Resume r, int index) {
+        storage.add(r);
+    }
+
+    @Override
+    protected void eraseResume(int index) {
+        storage.remove(index);
+    }
+}
