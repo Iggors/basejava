a9dd6df7785deef12c06c796d905603af7c736f7
Igor Kraev <ivk.spb@yandex.ru>
2022-03-25 20:38:12 +0300

    Fix the formatting

--- src/ru/basejava/storage/Storage.java
+++ src/ru/basejava/storage/Storage.java
@@ -6,21 +6,20 @@ import ru.basejava.model.Resume;
  * Array based storage for Resumes
  */
 public interface Storage {
+    int size();
 
     void clear();
-
-    void update(Resume r);
-
-    void save(Resume r);
-
-    Resume get(String uuid);
-
-    void delete(String uuid);
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll();
 
-    int size();
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void update(Resume r);
+
+    void delete(String uuid);
 }
