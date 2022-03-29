3da7c7f470e7ede06518be071787c9e738f2de47
Igor Kraev <ivk.spb@yandex.ru>
2022-03-29 23:11:50 +0300

    Refactored for proper work with MapStorage class

--- src/ru/basejava/storage/AbstractStorage.java
+++ src/ru/basejava/storage/AbstractStorage.java
@@ -6,51 +6,53 @@ import ru.basejava.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
 
-    protected abstract int getIndex(String uuid);
+    protected abstract Object getIndex(String uuid);
 
-    protected abstract Resume getResume(int index);
+    protected abstract Resume getResume(Object index);
 
-    protected abstract void updateResume(Resume r, int index);
+    protected abstract void updateResume(Resume r, Object index);
 
-    protected abstract void saveResume(Resume r, int index);
+    protected abstract void saveResume(Resume r, Object index);
 
-    protected abstract void eraseResume(int index);
+    protected abstract void eraseResume(Object index);
 
-    private int receiveIndexIfExist(String uuid) {
-        int index = getIndex(uuid);
+    protected abstract boolean doesResumeExist(Object index);
 
-        if (index >= 0) {
+    private Object receiveIndexIfExist(String uuid) {
+        Object index = getIndex(uuid);
+
+        if (doesResumeExist(index)) {
             throw new ExistStorageException(uuid);
         }
         return index;
     }
 
-    private int receiveIndexIfNotExist(String uuid) {
-        int index = getIndex(uuid);
+    private Object receiveIndexIfNotExist(String uuid) {
+        Object index = getIndex(uuid);
 
-        if (index < 0) {
+        if (!doesResumeExist(index)) {
             throw new NotExistStorageException(uuid);
         }
         return index;
     }
 
     public void save(Resume r) {
-        int index = receiveIndexIfExist(r.getUuid());
+        Object index = receiveIndexIfExist(r.getUuid());
         saveResume(r, index);
     }
 
     public Resume get(String uuid) {
-        int index = receiveIndexIfNotExist(uuid);
+        Object index = receiveIndexIfNotExist(uuid);
         return getResume(index);
     }
 
     public void update(Resume r) {
-        int index = receiveIndexIfNotExist(r.getUuid());
+        Object index = receiveIndexIfNotExist(r.getUuid());
         updateResume(r, index);
     }
 
     public void delete(String uuid) {
-        int index = receiveIndexIfNotExist(uuid);
+        Object index = receiveIndexIfNotExist(uuid);
         eraseResume(index);
     }
 }
